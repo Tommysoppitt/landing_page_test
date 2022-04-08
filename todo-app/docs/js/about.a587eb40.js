f2d90ab9a78d5dc09ea999c25a93d34bf15b0a4e
Tom Soppitt (FG-773) <thomas.soppitt@bmwgroup.com>
2022-04-08 10:35:07 +0200

    g

---
+++ todo-app/docs/js/about.a587eb40.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktodo_app"]=self["webpackChunktodo_app"]||[]).push([[443],{4335:function(n,t,a){a.r(t),a.d(t,{default:function(){return d}});var u=a(3396);const e={class:"about"},o=(0,u._)("h1",null,"This is an about page",-1),r=[o];function s(n,t){return(0,u.wg)(),(0,u.iD)("div",e,r)}var c=a(89);const i={},p=(0,c.Z)(i,[["render",s]]);var d=p}}]);
+//# sourceMappingURL=about.a587eb40.js.map
